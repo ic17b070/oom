1ca30bf9dd56ab8f0e469b0f739106efe24edeea
Holaygrail <stefan@nedic.at>
2018-03-04 13:39:35 +0100

    Task 2: creating the class

---
+++ tasks/Task2/Task2/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task2
+{
+    enum Unit
+    {
+        PIECE = 1,
+        LITER = 2,
+        GRAMM = 3
+    }
+
+    class Product
+    {
+        private String label;
+        private Double price;
+        private Int32 quantity;
+        private Unit p_unit;
+
+        public Decimal Label { get; }
+        public Double Price { get; }
+        public Int32 Quantity { get; }
+        public Unit Unit { get; }
+
+        public Product(String label, Double m_price, Int32 quantity, Unit p_unit)
+        {
+            this.label = label;
+            this.price = m_price;
+            this.quantity = quantity;
+            this.p_unit = p_unit;
+        }
+    }
+}
---
+++ tasks/Task2/Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p1 = new Product("Tomaten", 1.20, 500, Unit.GRAMM);
+            Product p2 = new Product("Gurken", 0.90, 1, Unit.PIECE);
+        }
+    }
+}
